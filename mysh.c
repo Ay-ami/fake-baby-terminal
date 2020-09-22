a64f71d82ce20745d2202855e7154c62b3db1c08
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2020-09-21 22:33:01 -0400

    Add files via upload

--- mysh.c
+++ mysh.c
@@ -9,13 +9,14 @@
 
 
 #define MAX_CHAR 300
-#define MAX_INPUTS 10
+#define MAX_STRINGS 10
 char* currDir;
 char** historyList;
-int historyLen = 0, historyPointer=0;
+int historyLen = 0, historyPointer=0, sizeOfHistory=10;
 
-enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, start = 5, background = 6, exterminate = 7, historyc = 8}; //, repeat =8};
+enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, start = 5, background = 6, exterminate = 7}; //, repeat =8};
 
+        // allocates space for an array of strings
         char** allocStrArray(int stringsize, int totalStrings)
         {
             char **array = malloc(totalStrings * sizeof(char *));
@@ -35,39 +36,39 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
 
         void addToHistory(char* str)
         {
-            if (historyLen < 10)
+            if (historyLen < sizeOfHistory) // if history is not full, add at the next open space
             {
                 strcpy(historyList[historyLen], str);
                 historyLen++;
             }
-            else
+            else // if history is full, start overriding starting from 0
             {
                 strcpy(historyList[historyPointer], str);
                 historyPointer++;
-                if(historyPointer==10)
+                if(historyPointer == sizeOfHistory) // once historyPointer has made the rounds though the whole array, it loops back to element 0
                     historyPointer = 0;
             }
-            
-
         }
-        char** tokenizer(int *numTokens)
+        void input(char* str)
         {
-            char* str = allocCharArray(MAX_CHAR);
+            printf("# ");
+            fgets(str, MAX_CHAR, stdin); // stdin = keyboard
+            addToHistory(str);
+            //str[sizeof(str)/sizeof(char)]= '\0';
+        }
+        char** tokenizer(char* str, int *numTokens)
+        {
             char* token = allocCharArray(MAX_CHAR);
             char* delim = " ";
-            char** returnTokens = allocStrArray(MAX_INPUTS, MAX_CHAR);
+            char** returnTokens = allocStrArray(MAX_CHAR, MAX_STRINGS);
             int count = 0;
-            
-            fgets(str, MAX_CHAR, stdin); // stdin = keyboard
-
-            addToHistory(str);
             
             // prints at least 1 token, keeps going if more exist
             token = strtok(str, delim);
             while (token != NULL)
             {
                 strcpy(returnTokens[count], token);
-                printf("token: %s\n", token);
+                // printf("token: %s\n", token); // check if the input has passed prpoerly
                 token = strtok(NULL, delim); // all other tokens need to have "NULL" here instead of the original string (idk why)
 
                 if (count == 10)
@@ -79,14 +80,11 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
 
             }
             *numTokens = count;
+            // printf("number of tokens: %d\n", *numTokens);
 
-            // gets rid of extranious '\0'
-            int i;
-            for (i = 1; i < count; i++)
-            {
-                returnTokens[i][strlen(returnTokens[i])-1] = '\0';
-            }
-                
+            // get rid of the newline character in the last input
+            returnTokens[count-1][strlen(returnTokens[count-1])-1] = '\0';
+            
             free(str);free(token);
 
             return returnTokens;
@@ -99,13 +97,11 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
 
             if (strcmp(command, "movetodir\0") == 0)
                 return movetodir;
-            else if (strcmp(command, "whereami\n") == 0)
+            else if (strcmp(command, "whereami\0") == 0)
                 return whereami;
-            else if ( strcmp(command, "history\n") == 0) // show history
+            else if ( strcmp(command, "history\0") == 0)
                 return history;
-            else if (strcmp(command, "history\0-\0") == 0) // clear history
-                return historyc;
-            else if (strcmp(command, "byebye\n") == 0)
+            else if (strcmp(command, "byebye") == 0)
                 return byebye;
             else if (strcmp(command, "start\0") == 0)
                 return start;
@@ -117,12 +113,10 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
             {
                 printf("command not recognied\n");
                 return 0;
-            }
-
-                
+            }                
         }
 
-        char* whereAmIf() // returns current directory
+        char* whereAmIf() // prints current directory
         {
             if (currDir == NULL)
                 perror("getcwd() error");
@@ -143,17 +137,19 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
         char* moveToDirf(char* newDir)
         {
             char* temp = allocCharArray(MAX_CHAR); 
+            //strcat(newDir, "/");
             strcpy(temp, currDir);
+            //printf("newDir: %s\n", newDir); // check the new directory has passed correctly 
 
             if (newDir[0] != '/') // if it doesn't already have a slash, put one
                 strcat(temp, "/");
 
             strcat(temp, newDir);
             
-            if( chdir (temp) == 0)
+            if( chdir (temp) == 0 )
             {
                 strcpy(currDir, temp);
-                printf("sucessfully changed to %s\n", currentDirectory());
+                printf("sucessfully changed to: %s\n", currentDirectory());
             }
             else
             {
@@ -168,24 +164,26 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
         // no difference. this took me more a long time to logic out on paper than i should have spent on this homework :_)
         void historyf() 
         {
-            int i, j = 1;
-            if (historyLen == 10) // if there are now 10 items in history and we've started overwriting them from the top
+            printf("Last %d inputs:\n---------------------------\n", historyLen);
+            int i, j = 0;
+            if (historyLen == sizeOfHistory) // if there are now 10 items in history and we've started overwriting them from the top
             {
                 for ( i = historyPointer; i < historyLen; i++)
                 {
-                    printf("%d %s\n", j++, historyList[i]);
+                    printf("%d %s\n", historyLen-j++, historyList[i]);
                 }
                 for ( i = 0; i < historyPointer; i++)
                 {
-                    printf("%d %s\n", j++, historyList[i]);
+                    printf("%d %s\n", historyLen-j++, historyList[i]);
                 }
             }else // if nothing has been overwritten yet because there are 10 or less items in history 
             {
                 for (i = 0 ; i < historyLen ; i++)
                 {
-                    printf("%d %s \n", j++, historyList[i]);
+                    printf("%d %s \n", historyLen-j++, historyList[i]);
                 }
             }
+            printf("---------------------------\n");
             
         }
         void clearHistory(char* c)
@@ -199,6 +197,7 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
                 }
                 historyLen = 0;
                 historyPointer = 0;
+                printf("history cleared\n");
             }
             else
             {
@@ -208,22 +207,48 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
         }
         void startf(char** tokenized, int numTokens) // start program [parameters]
         {
-            // seperate out the program name from the rest of the parameters
-            char* programName = allocCharArray(MAX_CHAR);
-            strcpy(programName, tokenized[1]);
 
             // seperate out all of the arguments including the program name but leave out the command "start"
-            char** args = allocStrArray(MAX_INPUTS, MAX_CHAR);
+            char** args = allocStrArray(MAX_CHAR, MAX_STRINGS);
             int i;
             for (i = 1 ; i < numTokens ; i++)
             {
-                strcpy(args[i], tokenized[i]);
+                strcpy(args[i-1], tokenized[i]);
             }
-            args[i] = NULL;
+            args[i-1] = NULL; // srgs must be null terminated
             
+            /*
+            printf("check the contents of the args:\n");
+             for(i=0 ; i < numTokens-1 ; i++)
+            {
+                 printf("%d: %s\n", i, args[i]);
+            }
+            */
+
             pid_t pid;
             int status;
 
+            /*
+            switch (pid = fork())
+            {
+                case -1:
+                    // On error fork() returns -1.
+                    perror("fork failed");
+                    exit(EXIT_FAILURE);
+                case 0:
+                    // On success fork() returns 0 in the child.
+                    printf(" CHILD <%ld> I'm alive! My PID is <%ld> and my parent got PID <%ld>.\n", (long) getpid(), (long) getpid(), (long) getppid());
+                    printf(" CHILD <%ld> Goodbye!\n", (long) getpid());
+                    exit(EXIT_SUCCESS);
+                default:
+                    // On success fork() returns the pid of the child to the parent.
+                    printf("PARENT <%ld> Spawned a child with PID = %ld.\n", (long) getpid(), (long) pid);
+                    wait(NULL);
+                    printf("PARENT <%ld> Child with PID = %ld terminated.\n", (long) getpid(), (long) pid);
+                    printf("PARENT <%ld> Goodbye.\n", (long) getpid());
+                    exit(EXIT_SUCCESS);
+            }
+            */
             pid = fork();
             if ( pid < 0)
             {
@@ -231,37 +256,32 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
             }
             else if (pid == 0) // child
             {
-                printf("Child PID is %ld\n", (long) getpid());
-                if (execvp(programName, args) < 0) {     /* execute the command  */
-                    printf("*** ERROR: exec failed\n");
+                if (execvp(args[0], args) < 0) {     // execute the command                                 // ok so here, i have a problem with args, it doesn't seem to pass though 
+                    printf("execution failed\n");
                     exit(1);
                 }
             }
             else // parent
             {
                while (wait(&status) != pid);
-               printf("not waiting no more\n");
+               printf("parent no longer waiting on child of PID %ld\n", (long) pid);
             }
             
         }
         void backgroundf(char** tokenized, int numTokens)
         {
-            // seperate out the program name from the rest of the parameters
-            char* programName = allocCharArray(MAX_CHAR);
-            strcpy(programName, tokenized[1]);
-
             // seperate out all of the arguments including the program name but leave out the command "start"
-            char** args = allocStrArray(MAX_INPUTS, MAX_CHAR);
+            char** args = allocStrArray(MAX_CHAR, MAX_STRINGS);
             int i;
             for (i = 1 ; i < numTokens ; i++)
             {
-                strcpy(args[i], tokenized[i]);
+                strcpy(args[i-1], tokenized[i]);
             }
-            args[i] = NULL;
+            args[i-1] = NULL;
             
             pid_t pid;
-            int status;
-
+            //int status;
+            
             pid = fork();
             if ( pid < 0)
             {
@@ -269,46 +289,54 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
             }
             else if (pid == 0) // child
             {
-                printf("Child PID is %ld\n", (long) getpid());
-                if (execvp(programName, args) < 0) {     /* execute the command  */
-                    printf("*** ERROR: exec failed\n");
+                if (execvp(args[0], args) < 0) {     // execute the command
+                    printf("execution failed\n");
                     exit(1);
                 }
-                printf("# ");
             }
             else // parent behaves differently if its a background process rather than a regular exicution
             {
-               while (waitpid(-1, &status, WNOHANG));
-               printf("not waiting no more\n");
+               printf("parent started child with PID of %ld\n", (long)pid);
+               //return;
             }
         }
         void exterminatef(char* input)
         {
-            int PID = atoi(input);
+            pid_t PID = atoi(input);
             if (kill(PID, SIGKILL) == -1)
             {
                 printf("kill error\n");
             }
-            printf("killed PID %d\n", PID);
+            printf("killed PID %ld\n", (long) PID);
         }
 
         int main()
         {
                 int exit = 0;
-                printf("welcome to fake baby shell\n");
+                printf("welcome to myShell\n");
                 currDir = allocCharArray(MAX_CHAR);
                 currDir = currentDirectory();
-                historyList = allocStrArray(MAX_INPUTS, MAX_CHAR);
+                historyList = allocStrArray(MAX_CHAR, MAX_STRINGS);
                 
                 do{
-                    char** tokenized = allocStrArray(MAX_INPUTS, MAX_CHAR);
+                    printf("inside of do/while loop\n");
+                    char** tokenized = allocStrArray(MAX_CHAR, MAX_STRINGS);
                     int *numTokens = (int*) calloc(1, sizeof(int));
-                    char* command = (char *)calloc(MAX_INPUTS, sizeof(char));
+                    char* command = allocCharArray(MAX_CHAR);//(char *)calloc(MAX_STRINGS, sizeof(char));
+                    char* str = allocCharArray(MAX_CHAR);
 
-                    printf("# ");
-                    tokenized = tokenizer(numTokens);
+                    input(str);
+
+                    tokenized = tokenizer(str, numTokens);
                     strcpy(command, tokenized[0]); // first token is always a command
                     /* movetodir = 1, whereami = 2, history  = 3, byebye = 4, start = 5, background = 6, exterminate = 7*/
+
+                    // printf("lets see the tokens in main: \n");
+                    // int i;
+                    // for (i=0; i < *numTokens; i++)
+                    // {
+                    //     printf("%d: %s\n", i, tokenized[i]);
+                    // }
 
                     switch (commandNumber(command))
                     {
@@ -319,10 +347,10 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
                                 whereAmIf();
                                 break;
                         case history:
-                                historyf();
-                                break;
-                        case historyc:
-                                clearHistory(tokenized[1]);
+                                if ( strcmp(tokenized[1], "") != 0 )
+                                    clearHistory(tokenized[1]);
+                                else
+                                    historyf();
                                 break;
                         case byebye:
                                 exit = 1;
@@ -332,6 +360,7 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
                                 break;
                         case background:
                                 backgroundf(tokenized, *numTokens);
+                                printf(">\n");
                                 break;
                         case exterminate:
                                 exterminatef(tokenized[1]);
@@ -339,10 +368,10 @@ enum supportedCommands{movetodir = 1, whereami = 2, history  = 3, byebye = 4, st
                         default:
                             break;
                     }
-                    
+                    //printf("bottom of switch\n");
 
                 }while (exit == 0);
 
-                printf("made it to the end\n");
+                printf("end of program\n");
                 return 0;
         }
